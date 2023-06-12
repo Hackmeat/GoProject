a4031adca4482875c2cc2524798279c850be4ccc
Hackmeat <beulichn@yaho.de>
2023-06-12 22:20:37 +0200

    [PROJ-6] script for ps implemented

---
+++ start_project.ps1
@@ -0,0 +1,21 @@
+# Setting paths
+$OriginalPath = $PSScriptRoot
+$FrontendPath = $PSScriptRoot + "\frontend\"
+
+Set-Location -Path $FrontendPath
+
+# Start the process and capture the process object
+$process = Start-Process -FilePath "npm" -ArgumentList "start" -PassThru
+
+# Wait for the process to start
+Start-Sleep -Seconds 5
+
+# Output success or failure message
+if (!$process.HasExited) {
+    Write-Host "npm start executed successfully."
+} else {
+    $exitCode = $process.ExitCode
+    Write-Host "npm start failed with exit code: $exitCode"
+}
+
+Set-Location -Path $OriginalPath
