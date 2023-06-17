b8b5fe2160b52afadccf56c5d2ff1395020b1294
Hackmeat <beulichn@yahoo.de>
2023-06-17 12:47:07 +0200

    [PROJ-12] mid session save

--- lbackend.sh
+++ lbackend.sh
@@ -4,17 +4,32 @@
 # Set Variables
 original_directory=$(pwd)
 postgres_directory="$original_directory/backend/postgresql"
-container_name="postgresql"
-image_name="postgresql-image"
+container_postgresql="postgresql"
+image_postgresql="postgresql-image"
+
+hasura_directory="$original_directory/backend/hasura"
+container_hasura="hasura"
+image_hasura="hasura-image"
 
 # Change directory
 cd "$postgres_directory"
 
 # Check if container is running
-if [[ "$(docker ps -aq -f name=${container_name})" ]]; then
-    echo "Starting the container ${container_name}."
-    docker start ${container_name}
+if [[ "$(docker ps -aq -f name=${container_postgresql})" ]]; then
+    echo "Starting the container ${container_postgresql}."
+    docker start ${container_postgresql}
 else
-    echo "The container ${container_name} does not exist. Creating..."
-    docker run -d --name ${container_name} ${image_name}
+    echo "The container ${container_postgresql} does not exist. Creating..."
+    docker run -d --name ${container_postgresql} ${image_postgresql}
+fi
+
+# Change directory
+cd "$hasura_directory"
+
+if [[ "$(docker ps -aq -f name=${container_hasura})" ]]; then
+    echo "Starting the container ${container_hasura}."
+    docker start ${container_hasura}
+else
+    echo "The container ${container_hasura} does not exist. Creating..."
+    docker run -d --name ${container_hasura} ${image_hasura}
 fi
